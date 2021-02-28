72ddda329d3afae89337e4a615d775d47e52f8e0
turgut.karadag <turgut_kara_dag@hotmail.com>
2021-02-28 19:24:51 +0300

    video send to other person

--- public/script.js
+++ public/script.js
@@ -1,9 +1,19 @@
 const socket = io('/');
-const video = document.getElementById('video-grid')
+const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
     host: '/',
     port: '3001'
 });
+
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true
+}).then(stream => {
+    addVideoStream(myVideo, stream)
+})
 
 myPeer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id)
@@ -13,3 +23,12 @@ myPeer.on('open', id => {
 socket.on('user-connected', userId => {
     console.log("user connected: " + userId)
 })
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    })
+    videoGrid.append(video)
+
+}
